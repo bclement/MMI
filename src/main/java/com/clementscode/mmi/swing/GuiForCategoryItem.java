6ffa7ffe93eacf64b548d1c06d2344477a3bc7d4
Matt Payne <Payne@MattPayne.org>
2011-05-18 08:24:48 -0500

    Added file browse buttons.  Still needs serious layout work!!!

--- src/main/java/com/clementscode/mmi/swing/GuiForCategoryItem.java
+++ src/main/java/com/clementscode/mmi/swing/GuiForCategoryItem.java
@@ -1,20 +1,40 @@
 package com.clementscode.mmi.swing;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class GuiForCategoryItem extends JPanel {
+public class GuiForCategoryItem extends JPanel implements ActionListener {
 
+	private static final long serialVersionUID = 1L;
 	private ActionListener actionListender;
+	private JTextField tfRepeatCount;
+	private JTextField tfImageFileName;
+	private ImageIcon imageIcon;
 
 	public GuiForCategoryItem(ActionListener al) {
 		super();
 		this.actionListender = al;
-		JTextField tfRepeatCount = new JTextField();
+		tfRepeatCount = new JTextField();
 		add(new LabelAndField("Repeats: ", tfRepeatCount));
+		tfImageFileName = new JTextField();
+		add(new LabelAndField("Image:", tfImageFileName));
+		imageIcon = new ImageIcon();
+		add(new JButton(imageIcon));
+		JButton browse = new JButton("Browse...");
+		browse.addActionListener(this);
+		add(browse);
+		JTextField tfAudioFileName = new JTextField();
+		add(new LabelAndField("Audio:", tfAudioFileName));
+		browse = new JButton("Browse...");
+		browse.addActionListener(this);
+		add(browse);
 		addButton("Add Another");
 		addButton("Delete");
 	}
@@ -23,6 +43,19 @@ public class GuiForCategoryItem extends JPanel {
 		JButton b = new JButton(string);
 		b.addActionListener(actionListender);
 		add(b);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"JPG & GIF Images", "jpg", "gif");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: "
+					+ chooser.getSelectedFile().getName());
+		}
+
 	}
 
 }
