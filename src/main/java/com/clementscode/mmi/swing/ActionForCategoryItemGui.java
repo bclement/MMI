6af4fb0deba0c4b769b483f79956fb99fc03f1b3
Matt Payne <Payne@MattPayne.org>
2011-06-04 12:36:53 -0500

    Add another button in CrudFrame now adds below the current category item.

--- src/main/java/com/clementscode/mmi/swing/ActionForCategoryItemGui.java
+++ src/main/java/com/clementscode/mmi/swing/ActionForCategoryItemGui.java
@@ -1,5 +1,6 @@
 package com.clementscode.mmi.swing;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
@@ -20,14 +21,23 @@ public class ActionForCategoryItemGui extends AbstractAction {
 
 	public void actionPerformed(ActionEvent e) {
 		CrudFrame crudFrame = guiForCategoryItem.getCrudFrame();
-
+		JPanel diyTable = crudFrame.getDiyTable();
 		if (CrudFrame.ADD_CATEGORY_ITEM.equals(cmd)) {
 
 			GuiForCategoryItem g4ci = new GuiForCategoryItem(crudFrame);
 			crudFrame.getLstGuiForCategoryItems().add(g4ci);
-			crudFrame.getDiyTable().add(g4ci);
+			int oneBellow = 0;
+			int numComponents = diyTable.getComponentCount();
+			for (int n = 0; n < numComponents; ++n) {
+				Component comp = diyTable.getComponent(n);
+				if (comp.equals(guiForCategoryItem)) {
+					oneBellow = n + 1;
+					break;
+				}
+			}
+			crudFrame.getDiyTable().add(g4ci, oneBellow);
 		} else if (CrudFrame.DELETE_CATEGORY_ITEM.equals(cmd)) {
-			JPanel diyTable = crudFrame.getDiyTable();
+
 			diyTable.remove(guiForCategoryItem);
 		}
 		crudFrame.refreshGui();
