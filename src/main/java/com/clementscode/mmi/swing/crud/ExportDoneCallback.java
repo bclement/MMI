f249b1c8d9ada8fbb369d153ce5f9c3c72a17f4f
Matt Payne <Payne@MattPayne.org>
2011-06-29 03:06:49 -0500

    Why check un-done things in!  My hard drive will never crash.

--- src/main/java/com/clementscode/mmi/swing/crud/ExportDoneCallback.java
+++
@@ -1,9 +0,0 @@
-package com.clementscode.mmi.swing.crud;
-
-import javax.swing.JComponent;
-
-public interface ExportDoneCallback {
-	public void exportDone(String overWrittenComponent, JComponent source2);
-
-	public void importData(JComponent comp);
-}
